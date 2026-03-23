c75eda1a9320738c7acaecafb42954968f2a1787
tanviruman <ruman777ahmed@gmail.com>
2026-03-23 20:05:53 +0600

    Refactor pattern3 class to format output with leading spaces and remove unused Main class and configuration files

--- Challenge-1/src/pattern3.java
+++ Challenge-1/src/pattern3.java
@@ -2,10 +2,10 @@ public class pattern3 {
 
     public static void main(String[] args) {
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
+        System.out.println("     *");
+        System.out.println("    **");
+        System.out.println("   ***");
+        System.out.println("  ****");
+        System.out.println(" *****");
     }
 }
