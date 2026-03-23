12c61556e28e09257be48011c920abb965040487
tanviruman <ruman777ahmed@gmail.com>
2026-03-23 13:22:40 +0600

    Update greeting message in main method to "good morning"

--- Online Class/src/main2.java
+++ Online Class/src/main2.java
@@ -2,7 +2,7 @@ public class main2 {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("good morning");
 
     }
 }
