365198f42281c549b1f735114a644817c784acde
tanviruman <ruman777ahmed@gmail.com>
2026-03-23 13:18:09 +0600

    Implement main method to display welcome message

--- Online Class/src/main2.java
+++ Online Class/src/main2.java
@@ -1,2 +1,8 @@
 public class main2 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
 }
