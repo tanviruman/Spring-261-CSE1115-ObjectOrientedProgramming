4d792a012ae08a8a0b65bc10697943f794f670c6
tanviruman <ruman777ahmed@gmail.com>
2026-03-23 19:51:31 +0600

    Add main method to pattern1 and pattern2 classes

--- Online Class/src/pattern1.java
+++ Online Class/src/pattern1.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class pattern1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
 }
--- Online Class/src/pattern2.java
+++ Online Class/src/pattern2.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class pattern2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
 }
