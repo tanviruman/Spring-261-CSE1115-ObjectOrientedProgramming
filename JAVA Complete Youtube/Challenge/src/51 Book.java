7970daf45161242e0bf5e5859f3e7fac050cf83f
tanviruman <ruman777ahmed@gmail.com>
2026-03-27 20:01:27 +0600

    Add initial implementation of Book class with title, author, and isbn attributes

---
+++ JAVA Complete Youtube/Challenge/src/51 Book.java
@@ -0,0 +1,23 @@
+public class Book {
+
+    String title;
+    String author;
+    String isbn;
+
+    static int totalBooks = 0;
+
+    void borrowBook(){
+
+
+
+    }
+
+    void returnBook(){
+
+
+        
+    }
+
+
+
+}
