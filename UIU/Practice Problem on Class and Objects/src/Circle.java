f102f35516cf23561b41b6c5a5b58f2afea0e4c4
tanviruman <ruman777ahmed@gmail.com>
2026-03-29 00:07:09 +0600

    Refactor Circle class to include radius property and methods for area and circumference calculations

--- UIU/Practice Problem on Class and Objects/src/Circle.java
+++ UIU/Practice Problem on Class and Objects/src/Circle.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+ class Circle {
+    double radius;
 
-public class Circle {
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return Math.PI * radius * radius;
+    }
+
+    double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(7);
+        System.out.println("Area: " + c.area());
+        System.out.println("Circumference: " + c.circumference());
+    }
 }
