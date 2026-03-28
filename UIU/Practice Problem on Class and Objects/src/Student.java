1b13c489ed1ab037f6511bdefcac34ddf256e24f
tanviruman <ruman777ahmed@gmail.com>
2026-03-28 22:47:37 +0600

    Add complete implementation of Student class with methods for total and average marks

--- UIU/Practice Problem on Class and Objects/src/Student.java
+++ UIU/Practice Problem on Class and Objects/src/Student.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+class Student {
+    String name;
+    String rollNumber;
+    double[] marks;
 
-public class Student {
+    Student(String name, String rollNumber, double[] marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    double totalMarks() {
+        double total = 0;
+        for (double m : marks) total += m;
+        return total;
+    }
+
+    double average() {
+        return totalMarks() / marks.length;
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("Bob", "R001", new double[]{80, 90, 75, 85});
+        System.out.println("Total: " + s.totalMarks());
+        System.out.println("Average: " + s.average());
+    }
 }
