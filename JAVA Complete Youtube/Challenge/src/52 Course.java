7eb129982dcae18cc98ae7abbe34607aa4185743
tanviruman <ruman777ahmed@gmail.com>
2026-03-28 18:11:30 +0600

    Add initial implementation of Main class with a greeting and loop

--- JAVA Complete Youtube/Challenge/src/52 Course.java
+++ JAVA Complete Youtube/Challenge/src/52 Course.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+/*Design a Course class.
+• Instance variables: courseName, enrolledStudents.
+• Static variable: maxCapacity, the maximum number of students for any
+course.
+• Instance methods: enrollStudent(String studentName),
+unenrollStudent(String studentName).
+• Static method: setMaxCapacity(int capacity), to set the maximum
+capacity for courses.*/
 
-public class Course {
+class Course {
+
+
 }
