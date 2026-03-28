4a573c71bbd86309ff3cdee15a55b38a5a836cfc
tanviruman <ruman777ahmed@gmail.com>
2026-03-29 00:03:25 +0600

    Add complete implementation of LibraryBook class with methods for borrowing and returning books

--- UIU/Practice Problem on Class and Objects/src/LibraryBook.java
+++ UIU/Practice Problem on Class and Objects/src/LibraryBook.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+ class LibraryBook {
+    String title;
+    String author;
+    boolean isAvailable;
 
-public class LibraryBook {
+    LibraryBook(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true; // available by default
+    }
+
+    void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(title + " has been borrowed.");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+
+    void returnBook() {
+        isAvailable = true;
+        System.out.println(title + " has been returned.");
+    }
+
+    public static void main(String[] args) {
+        LibraryBook book = new LibraryBook("Java Basics", "James Gosling");
+        book.borrowBook();
+        book.borrowBook(); // try borrowing again
+        book.returnBook();
+        book.borrowBook(); // available again
+    }
 }
