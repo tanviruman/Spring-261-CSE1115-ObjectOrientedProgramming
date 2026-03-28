5e1f7b874d3a55ec3fb97a00b93ad6304d4d14ce
tanviruman <ruman777ahmed@gmail.com>
2026-03-29 00:09:07 +0600

    Add complete implementation of Person class with address update and display methods

--- UIU/Practice Problem on Class and Objects/src/Person.java
+++ UIU/Practice Problem on Class and Objects/src/Person.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+class Person {
+    String name;
+    int age;
+    String address;
 
-public class Person {
+    Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    void updateAddress(String newAddress) {
+        address = newAddress;
+        System.out.println("Address updated to: " + address);
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Address: " + address);
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person("Diana", 25, "123 Main St");
+        p.displayDetails();
+        p.updateAddress("456 New Ave");
+        p.displayDetails();
+    }
 }
