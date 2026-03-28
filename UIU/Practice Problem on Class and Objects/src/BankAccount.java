9011141bc64c9205ff103660dc53db1d66818634
tanviruman <ruman777ahmed@gmail.com>
2026-03-28 20:15:15 +0600

    Refactor BankAccount class to include deposit and withdraw methods with balance checks

--- UIU/Practice Problem on Class and Objects/src/BankAccount.java
+++ UIU/Practice Problem on Class and Objects/src/BankAccount.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+class BankAccount {
+    String holderName;
+    String accountId;
+    double balance;
 
-public class BankAccount {
+    BankAccount(String holderName, String accountId, double balance) {
+        this.holderName = holderName;
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " | New Balance: " + balance);
+    }
+
+    void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient funds!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " | New Balance: " + balance);
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount("Alice", "ACC001", 1000.0);
+        acc.deposit(500);
+        acc.withdraw(200);
+        acc.withdraw(2000); // will show insufficient funds
+    }
 }
